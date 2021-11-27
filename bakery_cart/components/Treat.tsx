d13142a64c22cd416ca1da5a47ed0670422d960d
Derek <derekcrosslu@gmail.com>
2021-11-27 13:16:00 -0500

    fixing styling for treats component

--- bakery_cart/components/Treat.tsx
+++ bakery_cart/components/Treat.tsx
@@ -3,17 +3,21 @@ export default function Treat(props) {
 
     const item = 0
   return (
-    <div>
-      <img className="small" src={treat.imageURL} alt={treat.name}></img>
-      <h3>{treat.name}</h3>
-      <div>${treat.price}</div>
-      <div>
-        <button onClick={() => addToCart(treat)} className="add">
-          Add
-        </button>
-        <button onClick={() => removeFromCart(treat)} className="remove">
-          Remove
-        </button>
+    <div className="treats">
+      <div className="treat_images_text">
+        <img className="small" src={treat.imageURL} alt={treat.name}></img>
+        <div className="treats_buttons">
+          <button onClick={() => addToCart(treat)} className="add">
+            +
+          </button>
+          <button onClick={() => removeFromCart(treat)} className="remove">
+            -
+          </button>
+        </div>
+        <div className="treat_text">
+          <div>${treat.price}</div>
+          <h3>{treat.name}</h3>
+        </div>
       </div>
     </div>
   );
--- bakery_cart/components/Treats.tsx
+++ bakery_cart/components/Treats.tsx
@@ -3,19 +3,21 @@ export default function Treats(props) {
     
     const { treats, addToCart, removeFromCart } = props;
 
-    return (<main className="block col-2">
-        <h2>Treats</h2>
-        <div className="row">
-            {treats.map((treat) => {
-                return (
-                  <Treat
-                    key={treat.id}
-                    treat={treat}
-                    addToCart={addToCart}
-                    removeFromCart={removeFromCart}
-                  ></Treat>
-                );
-            })}
+    return (
+      <main className="block treats">
+        {/* <h2>Treats</h2> */}
+        <div className="row treats">
+          {treats.map((treat) => {
+            return (
+              <Treat
+                key={treat.id}
+                treat={treat}
+                addToCart={addToCart}
+                removeFromCart={removeFromCart}
+              ></Treat>
+            );
+          })}
         </div>
-    </main>)
+      </main>
+    );
 }
